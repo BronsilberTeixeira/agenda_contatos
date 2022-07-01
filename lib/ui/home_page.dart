2ae59b87e1e206bb406e61d1d1942218ddccde23
bronsilber <bron.teixeira.s@gmail.com.br>
2022-07-01 16:46:25 -0300

    criando a tela de cadastrar/editar contatos

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'package:agenda_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  // const HomePage({ Key? key }) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -12,28 +13,108 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
-  void initState(){
-    super.initState();
+    void initState() {
+      super.initState();
 
-    Contact c = Contact(); 
-
-    c.name = "Bronsilber";
-    c.email = "bron.teixeira@gmail.com";
-    c.phone = "31982430869";
-    c.img = "iamge teste";
-
-    print(c);
-
-    helper.getAllContacts().then((listContacts) {
-      print(listContacts);
-    });
-  }
+      _getAllContacts();
+    }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+       backgroundColor: Colors.blueGrey[100],
+       floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _showContactpage();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+        ),
+        body: ListView.builder(
+          padding: EdgeInsets.all(20),
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return _contactCard(context, index);
+          }
+        ),
     );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ? 
+                      FileImage(File(contacts[index].img)) :
+                        AssetImage("images/person.png")
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(contacts[index].name ?? "", 
+                      style: TextStyle(fontSize: 22, 
+                      fontWeight: FontWeight.bold),
+                    ),
+                     Text(contacts[index].email ?? "", 
+                      style: TextStyle(fontSize: 15),
+                    ),
+                     Text(contacts[index].phone ?? "", 
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+               ),
+              )
+            ],
+          ),
+        ),
+      ),
+      onTap: () {
+        _showContactpage(contact:  contacts[index]);
+      },
+    );
+  }
+
+  _getAllContacts() {
+       helper.getAllContacts().then((list) {
+        setState(() {
+          contacts = list;
+        });
+      });
+  }
+
+  void _showContactpage({Contact contact}) async{
+    final recContact = await Navigator.push(context, 
+      MaterialPageRoute(builder: (context) => ContactPage(contact: contact))
+    );
+    if(recContact != null) {
+      if(contacts != null){
+        await helper.updateContact(recContact);
+      }
+      else {
+        await helper.saveContact(recContact);
+      }
+        _getAllContacts();
+    }
   }
 }
